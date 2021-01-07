752957d864fde9c67f413f4091b51b719aec4d94
Higor Braga <higorbraga00@gmail.com>
2021-01-07 01:03:04 -0300

    Add routes and StepsHeader

---
+++ front-web/src/Orders/index.tsx
@@ -0,0 +1,12 @@
+import StepsHead from './StepsHeader';
+import './styles.css';
+
+function Orders() {
+    return (
+        <div className="orders-container">
+          <StepsHead / >
+        </div>
+    )
+}
+
+export default Orders;
