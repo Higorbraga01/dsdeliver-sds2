213e644fbcab9fdc3aaa8e82183692fb91994e4c
Higor Braga <higorbraga00@gmail.com>
2021-01-10 18:46:37 -0300

    Add orders list

--- front-mobile/src/Header/index.tsx
+++ front-mobile/src/Header/index.tsx
@@ -1,12 +1,21 @@
+import { useNavigation } from '@react-navigation/native';
 import React from 'react';
 import { Image, StyleSheet, Text, View } from 'react-native';
+import { TouchableWithoutFeedback } from 'react-native-gesture-handler';
 
 export default function Header() {
+  const navigation = useNavigation();
+
+  const handleOnpress = () => {
+    navigation.navigate('Home');
+  }
   return (
+    <TouchableWithoutFeedback onPress={handleOnpress}>
     <View style={styles.container}>
         <Image source={require('../assets/logo.png')}/>
       <Text style={styles.text}>DS Delivery</Text>
     </View>
+    </TouchableWithoutFeedback>
   );
 }
 
